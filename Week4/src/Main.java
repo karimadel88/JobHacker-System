42b950a9e720cf5f8afc0b583d6b669f40479b81
Karim Adel <ka565872@gmail.com>
2023-04-17 00:33:34 +0200

    Initial Commmit at the week4

---
+++ Week4/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String s=new String("Sachin");
+        //concat() method appends the string at the end
+        System.out.println(s.concat(" Tendulkar"));
+    }
+
+}
