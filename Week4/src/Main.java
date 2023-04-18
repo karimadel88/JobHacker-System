22d548d212ebf3f56867020eea9b5754f7baf74d
Karim Adel <ka565872@gmail.com>
2023-04-18 19:33:02 +0200

    Clean old files

--- Week4/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String s=new String("Sachin");
-        //concat() method appends the string at the end
-        System.out.println(s.concat(" Tendulkar"));
-    }
-
-}
